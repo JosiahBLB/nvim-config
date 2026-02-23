51a194e81442be61ef5ab5bbe949ac1f0c82665d
JBrough <josiah.brough@gspace.com>
2026-02-23 20:30:01 +1000

    Auto commit on 2026-02-23 20:30:01

--- lua/plugins/indent-line.lua
+++ lua/plugins/indent-line.lua
@@ -4,6 +4,21 @@ return {
     -- See `:help ibl`
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     main = 'ibl',
+    config = function()
+      vim.api.nvim_set_hl(0, 'Function', { fg = '#ff0000' })
+
+      require('ibl').setup {
+        indent = {
+          char = '▏',
+        },
+        scope = {
+          enabled = true,
+          show_start = true,
+          show_end = true,
+          highlight = { 'Function' },
+        },
+      }
+    end,
     opts = {
       indent = { char = '▏' },
       scope = {
